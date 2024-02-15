e3c69d80eb6a890e87786073b6019a83c00f8ca7
USER <sithiraroneth@gmail.com>
2024-02-15 18:03:16 +0530

    initial commit

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -14,8 +14,6 @@ public class Main {
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
-
-
         transaction.commit();
         session.close();
 
